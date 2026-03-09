f6c4aefa4d7b8ea4593112443e0d107280a2fd67
Gerardo Martínez Carbajal <gerardomartinezcarbajal@MacBook-Pro-de-Gerardo.local>
2026-03-09 10:36:09 -0600

    N

--- Lab17/util/database.js
+++ Lab17/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'Lab17G',
-    password: 'feFOfe43?'
+    password: ''
 });
 
 module.exports = pool.promise();
